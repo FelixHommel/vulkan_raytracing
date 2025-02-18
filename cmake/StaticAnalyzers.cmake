e47e615e2a583ad4939ad5bb8db137f1d6018ae4
FelixHommel <felix.hommel@uni-ulm.de>
2025-02-18 21:35:44 +0100

    feat: add code coverage support for cmake

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,9 +1,11 @@
 option(ENABLE_COVERAGE "Enable code coverage" OFF)
 
-if(NOT CMAKE_BUILD_TYPE STREQUAL "Release" OR ENABLE_COVERAGE)
-    if(ENABLE_COVERAGE)
+if(ENABLE_COVERAGE)
+    if(${CMAKE_CXX_COMPILER_ID} MATCHES "(Apple)?[Cc]lang")
         message(STATUS "Building with coverage instrumentation")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
+    else()
+        message(FATAL_ERROR "Code coverage can only be generated with clang compilers")
     endif()
 endif()
 
